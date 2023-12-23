55dc38bfddc381d20efcca6102325e1e3fc5f1fe
deshortone <jingshianggu@gmail.com>
2023-12-23 23:54:51 +0000

    feat: display all kanbans in alphabetical order

--- app/(auth)/select-board/page.tsx
+++ app/(auth)/select-board/page.tsx
@@ -1,7 +1,13 @@
 'use client'
 
-const SelectKanbanPage = () => {
+import prisma from "@/lib/prisma";
 
+const SelectKanbanPage = async () => {
+    const boards = await prisma.kanban.findMany({
+        orderBy: {
+            title: "asc"
+        }
+    })
 
     return (
         <main className="inline-flex items-center justify-center w-full h-screen">
@@ -15,18 +21,11 @@ const SelectKanbanPage = () => {
 
                 <div className="w-full">
                     <div className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                        <a href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                            kanbanboard1
-                        </a>
-                        <a href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                            kanbanboard2
-                        </a>
-                        <a href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                            kanbanboard3
-                        </a>
-                        <a href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                            kanbanboard4
-                        </a>
+                        {boards.map((board) => (
+                            <a href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+                                {board.title}
+                            </a>
+                        ))}
                     </div>
                 </div>
 
