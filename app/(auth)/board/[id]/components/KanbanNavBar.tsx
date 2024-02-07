37f176437220199a6463ffc7f0a8f48e6fc78703
deshortone <jingshianggu@gmail.com>
2024-02-07 18:10:55 +0000

    feat: added kanban navbar

---
+++ app/(auth)/board/[id]/components/KanbanNavBar.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation"
+
+interface prop {
+    title: string
+}
+
+export const KanbanNavBar = ({ title }: prop) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                    {title}
+                </span>
+
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href={usePathname() + "/settings"}
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >
+                                Setting
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
