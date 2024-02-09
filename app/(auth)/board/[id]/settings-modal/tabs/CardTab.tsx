49447a0c63a18c881e7fc05d47d02ef0ef814443
deshortone <jingshianggu@gmail.com>
2024-02-09 10:51:40 +0000

    feat: Settings card tab not using shadcn form
    reason being I have a table which will have 2 types of inputs, a group of radio buttons
    and a number of single line inputs (not grouped together) looking at documentation
    it doesn't seem possible to create the table whilst having individual input fields without
    having to nest several FormFields and I think that will be too messy/ ugly so resorting to
    simply using react-hook-form

--- app/(auth)/board/[id]/settings-modal/tabs/CardTab.tsx
+++ app/(auth)/board/[id]/settings-modal/tabs/CardTab.tsx
@@ -1,7 +1,6 @@
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
@@ -10,34 +9,45 @@ import {
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { TabsContent } from "@/components/ui/tabs"
 
+import { useForm } from "react-hook-form"
+import { Input } from "@/components/ui/input"
+
 export const CardTab = () => {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data)
 
     return (
         <TabsContent value="card">
-            <RadioGroup defaultValue="comfortable">
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[100px]">Default</TableHead>
-                            <TableHead>Name</TableHead>
-                            <TableHead>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <RadioGroup defaultValue="comfortable">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">Default</TableHead>
+                                <TableHead>Name</TableHead>
+                                <TableHead>
 
-                            </TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell className="font-medium">
-                                <RadioGroupItem value="default" id="r1" />
-                            </TableCell>
-                            <TableCell>Paid</TableCell>
-                            <TableCell className="text-right">
-                                <img src="/setting.svg" />
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </RadioGroup>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="font-medium">
+                                    <RadioGroupItem value="a" {...register("weather")} />
+                                </TableCell>
+                                <TableCell>
+                                    <Input {...register("test")} />
+                                </TableCell>
+                                <TableCell className="text-right">
+                                    <img src="/setting.svg" />
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </RadioGroup>
+                <input type="submit" />
+            </form>
         </TabsContent>
     )
 }
