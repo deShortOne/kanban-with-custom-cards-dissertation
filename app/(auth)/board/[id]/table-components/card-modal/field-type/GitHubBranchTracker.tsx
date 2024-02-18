9f1ce7039d7ed0d11868c6f5fa8423e92c546ae6
deshortone <jingshianggu@gmail.com>
2024-02-18 15:28:15 +0000

    feat: scroll and add and remove branches

--- app/(auth)/board/[id]/table-components/card-modal/field-type/GitHubBranchTracker.tsx
+++ app/(auth)/board/[id]/table-components/card-modal/field-type/GitHubBranchTracker.tsx
@@ -4,6 +4,10 @@ import { FieldTypeProp } from "./Base"
 import { useFieldArray, useFormContext } from "react-hook-form";
 import { useEffect, useState } from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+const loadingText = "...loading..."
 
 export const GitHubBranchTracker = ({ form, fieldTypeData, name }: FieldTypeProp) => {
 
@@ -18,25 +22,24 @@ export const GitHubBranchTracker = ({ form, fieldTypeData, name }: FieldTypeProp
         name: name + ".branches",
     })
 
+    const getBranchStatus = async (id: string, branchName: string) => {
+        const response = await fetch('/api/github/branch/status?'
+            + new URLSearchParams({
+                owner: "deShortOne", // TODO !!!!!!
+                repo: getValues()[name].repo,
+                branch: branchName
+            }), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+        const data = await response.json()
+        updateBranchStatuses(prevInfo => ({ ...prevInfo, ["a" + id.replaceAll("-", "")]: data }))
+    }
     const [branchStatus, updateBranchStatuses] = useState({ "a": "b" })
     useEffect(() => {
-        const getBranchStatus = async (id: string, branchName: string) => {
-            const response = await fetch('/api/github/branch/status?'
-                + new URLSearchParams({
-                    owner: "deShortOne", // TODO !!!!!!
-                    repo: getValues()[name].repo,
-                    branch: branchName
-                }), {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-
-            const data = await response.json()
-            updateBranchStatuses(prevInfo => ({ ...prevInfo, ["a" + id.replaceAll("-", "")]: data }))
-        }
-
         fields.forEach(async (i, idx) =>
             branchStatus.hasOwnProperty("a" + i.id.replaceAll("-", ""))
                 ?
@@ -51,46 +54,57 @@ export const GitHubBranchTracker = ({ form, fieldTypeData, name }: FieldTypeProp
         <FormItem className="flex flex-col">
             <FormLabel>{label}</FormLabel>
             <Input {...register(name + ".repo")} className="w-48" />
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[20vw]">Feature title</TableHead>
-                        <TableHead className="w-[20vw]">Branch name</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead className="w-[10vw]" />
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {fields.map((field, index) => (
+            <ScrollArea className="h-[50vh]">
+                <Table>
+                    <TableHeader>
                         <TableRow>
-                            <TableCell>
-                                <Input key={field.id}
-                                    {...register(name + `.branches.${index}.title`)}
-                                    className="font-medium"
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <Input
-                                    key={field.id}
-                                    {...register(name + `.branches.${index}.branchName`)}
-                                    className="font-medium aria-readonly"
-                                />
-                            </TableCell>
-                            <TableCell>
-                                {branchStatus.hasOwnProperty("a" + field.id.replaceAll("-", ""))
-                                    ?
-                                    branchStatus["a" + field.id.replaceAll("-", "")]
-                                    :
-                                    "...loading..."
-                                }
-                            </TableCell>
-                            <TableCell>
-                                Icon
-                            </TableCell>
+                            <TableHead className="w-[20vw]">Feature title</TableHead>
+                            <TableHead className="w-[20vw]">Branch name</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead className="w-[10vw]" />
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                    </TableHeader>
+                    <TableBody>
+                        {fields.map((field, index) => (
+                            <TableRow>
+                                <TableCell>
+                                    <Input key={field.id}
+                                        {...register(name + `.branches.${index}.title`)}
+                                        className="font-medium"
+                                        placeholder="feature name"
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <Input
+                                        key={field.id}
+                                        {...register(name + `.branches.${index}.branchName`)}
+                                        className="font-medium aria-readonly"
+                                        placeholder="branch name"
+                                        onBlur={() => {
+                                            updateBranchStatuses(prevInfo => ({ ...prevInfo, ["a" + field.id.replaceAll("-", "")]: loadingText }))
+                                            getBranchStatus(field.id, getValues()[name].branches[index].branchName)
+                                        }}
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    {branchStatus.hasOwnProperty("a" + field.id.replaceAll("-", ""))
+                                        ?
+                                        branchStatus["a" + field.id.replaceAll("-", "")]
+                                        :
+                                        loadingText
+                                    }
+                                </TableCell>
+                                <TableCell>
+                                    <button onClick={() => remove(index)}>
+                                        <img src="/delete.svg" />
+                                    </button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </ScrollArea>
+            <Button onClick={() => append({})} type="button">Add</Button>
         </FormItem>
     )
 }
