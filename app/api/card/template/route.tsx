5d9bef6a633f5427efbf844fdbcba0353a5b4a49
deshortone <jingshianggu@gmail.com>
2024-02-19 20:26:09 +0000

    feat: get field types and card template

---
+++ app/api/card/template/route.tsx
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function GET(req: Request) {
+    const url = new URL(req.url)
+    const cardTemplateId = url.searchParams.get("cardTemplateId")!
+
+    const cardTemplate = await prisma.cardTemplate.findFirst({
+        where: {
+            id: parseInt(cardTemplateId),
+        },
+        include: {
+            tabs: {
+                include: {
+                    tabFields: {
+                        include: {
+                            fieldType: true
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    return NextResponse.json(cardTemplate);
+}
