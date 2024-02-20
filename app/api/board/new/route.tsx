fab7d99b7fe1a0ac23ebb10b9ba72d890851107a
deshortone <jingshianggu@gmail.com>
2024-02-20 14:24:17 +0000

    feat: fill in initial data for new boards

--- app/api/board/new/route.tsx
+++ app/api/board/new/route.tsx
@@ -4,40 +4,184 @@ import { OPTIONS } from '@/app/api/auth/[...nextauth]/route'
 import { prisma } from "@/lib/prisma"
 
 export async function POST(request: Request) {
-  const formData = await request.formData()
+    const formData = await request.formData()
 
-  const kanban = await prisma.kanban.create({
-    data: {
-      title: formData.get("name") as string
+    const kanban = await setBoardInitialData(formData.get("name") as string)
+
+    const id = await getUserId();
+    if (id == -1) {
+        return Response.json({ status: false })
     }
-  })
 
-  const id = await getUserId();
-  if (id == -1) {
-    return Response.json({ status: false })
-  }
-  
-  await prisma.userRoleKanban.create({
-    data: {
-      userId: id,
-      kanbanId: kanban.id,
-      permission: Role.EDITOR
-    }
-  })
+    await prisma.userRoleKanban.create({
+        data: {
+            userId: id,
+            kanbanId: kanban.id,
+            permission: Role.EDITOR
+        }
+    })
 
-  return Response.json({ kanban })
+    return Response.json({ kanban })
+}
+
+async function setBoardInitialData(name: string) {
+    const kanban = await prisma.kanban.create({
+        include: {
+            KanbanColumns: true,
+            KanbanSwimLanes: true,
+            CardTemplate: {
+                include: {
+                    tabs: {
+                        include: {
+                            tabFields: {
+                                include: {
+                                    fieldType: true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        data: {
+            title: name,
+            KanbanColumns: {
+                createMany: {
+                    data: [
+                        { title: "To do", order: 1, },
+                        { title: "Doing", order: 1, },
+                        { title: "Done", order: 2 },
+                    ],
+                }
+            },
+            KanbanSwimLanes: {
+                createMany: {
+                    data: [
+                        { title: "Team 1", order: 1, },
+                        { title: "Team 2", order: 2, },
+                    ]
+                }
+            },
+            CardTemplate: {
+                create: [{
+                    name: "task",
+                    version: 1,
+                    cardTypeId: 1,
+                    tabs: {
+                        create: [{
+                            name: "Base information",
+                            order: 1,
+                            sizeX: 1,
+                            sizeY: 2,
+
+                            tabFields: {
+                                create: [{
+                                    posX: 1,
+                                    posY: 1,
+                                    fieldTypeId: 1,
+                                    data: "Short description"
+                                },
+                                {
+                                    posX: 1,
+                                    posY: 2,
+                                    fieldTypeId: 2,
+                                    data: "Extended description",
+                                }]
+                            }
+                        },
+                        {
+                            name: "Github",
+                            order: 2,
+                            sizeX: 1,
+                            sizeY: 1,
+
+                            tabFields: {
+                                create: {
+                                    posX: 1,
+                                    posY: 1,
+                                    fieldTypeId: 1,
+                                    data: "To be done",
+                                }
+                            }
+                        }],
+                    }
+                },
+                {
+                    name: "bug",
+                    version: 1,
+                    cardTypeId: 2,
+                    tabs: {
+                        create: [{
+                            name: "Error",
+                            order: 1,
+                            sizeX: 2,
+                            sizeY: 2,
+
+                            tabFields: {
+                                create: [{
+                                    posX: 1,
+                                    posY: 1,
+                                    fieldTypeId: 2,
+                                    data: "Steps to reproduce error",
+                                },
+                                {
+                                    posX: 1,
+                                    posY: 2,
+                                    fieldTypeId: 1,
+                                    data: "Actual results",
+                                },
+                                {
+                                    posX: 2,
+                                    posY: 2,
+                                    fieldTypeId: 1,
+                                    data: "Expected result",
+                                }]
+                            }
+                        }]
+                    }
+                }],
+            },
+        }
+    })
+
+    await prisma.card.create({
+        data: {
+            kanbanId: kanban.id,
+            title: "Start here",
+            columnId: -1,
+            swimLaneId: -1,
+            cardTemplateId: kanban.CardTemplate[0].id,
+            order: 1,
+            allTabsFieldInformation: {
+                create: [{
+                    cardTemplateTabFieldId: kanban.CardTemplate[0].tabs[0].tabFields[0].id,
+                    data: "Create new cards by clicking New Task above the card"
+                },
+                {
+                    cardTemplateTabFieldId: kanban.CardTemplate[0].tabs[0].tabFields[1].id,
+                    data: "You can create Bug cards by clicking on the down arrow next to New Task\nAlternatively, (not yet implemented), you will be able to create new formats inside the card"
+                },
+                {
+                    cardTemplateTabFieldId: kanban.CardTemplate[0].tabs[1].tabFields[0].id,
+                    data: "This is to be implemented in the future"
+                }]
+            }
+        }
+    })
+
+    return kanban
 }
 
 async function getUserId() {
-  // ideally I should be pulling from an api, but I couldn't get it to work
-  const session = await getServerSession(OPTIONS);
-  if (!session || !session.user) {
-    return -1
-  }
-  const user = await prisma.user.findUniqueOrThrow({
-      where: {
-          email: session.user.email as string
-      }
-  })
-  return user.id
+    // ideally I should be pulling from an api, but I couldn't get it to work
+    const session = await getServerSession(OPTIONS);
+    if (!session || !session.user) {
+        return -1
+    }
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: session.user.email as string
+        }
+    })
+    return user.id
 }
