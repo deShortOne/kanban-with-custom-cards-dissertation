e67bc0de3d5043d256b70e97bd2487e6a3ed74e5
deshortone <jingshianggu@gmail.com>
2024-02-21 09:22:21 +0000

    build: added FieldProp

--- app/(auth)/card/[id]/component/Base.tsx
+++ app/(auth)/card/[id]/component/Base.tsx
@@ -1,27 +1,27 @@
 
-export interface DataProp {
-    id: number,
-    name: string,
-    tabs: {
-        name: string,
-        order: number,
-        sizeX: number,
-        sizeY: number,
-        tabFields: {
-            data: string,
-            posX: number,
-            posY: number,
-            fieldType: {
-                id: number,
-                name: string,
-                description: string,
-            },
-        }[],
-    }[],
+export interface FieldTypeProp {
+    id: number
+    name: string
+    description: string
 }
 
-export interface FieldTypeProp {
-    id: number,
-    name: string,
-    description: string,
+export interface FieldProp {
+    data: string
+    posX: number
+    posY: number
+    fieldType: FieldTypeProp
+}
+
+export interface Tab {
+    name: string
+    order: number
+    sizeX: number
+    sizeY: number
+    tabFields: FieldProp[]
+}
+
+export interface DataProp {
+    id: number
+    name: string
+    tabs: Tab[]
 }
