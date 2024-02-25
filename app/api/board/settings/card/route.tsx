816d5ae41fa9ffcdb1bc764302ea32abd8383ce3
deshortone <jingshianggu@gmail.com>
2024-02-25 13:57:20 +0000

    feat: api updates default card

--- app/api/board/settings/card/route.tsx
+++ app/api/board/settings/card/route.tsx
@@ -44,32 +44,26 @@ export async function GET(request: Request) {
 export async function POST(req: Request) {
     const data = await req.json()
 
-    const kanbanId = data.kanbanId
-    delete data["kanbanId"]
-    const defaultCardId = data.isDefault
-    delete data["isDefault"]
+    const kanbanId = parseInt(data.kanbanId)
+    const defaultCardId = parseInt(data.isDefault)
 
-    const dataInArray: any = {}
-    for (var i in data) {
-        const idAndDataType = i.split("-")
-        if (!dataInArray.hasOwnProperty(idAndDataType[0])) {
-            dataInArray[idAndDataType[0]] = {}
+    await prisma.cardTemplate.updateMany({
+        where: {
+            kanbanId: kanbanId,
+        },
+        data: {
+            isDefault: false,
         }
-        if (idAndDataType[1] === "card") {
-            dataInArray[idAndDataType[0]]["name"] = data[i]
-        } else if (idAndDataType[1] === "cardType") {
-            // no actual idea what it means to change the card type... -_-
-        }
-    }
+    })
 
-    for (var i in dataInArray) {
-        await prisma.cardTemplate.update({
-            where: {
-                id: parseInt(i),
-            },
-            data: dataInArray[i],
-        })
-    }
+    await prisma.cardTemplate.update({
+        where: {
+            id: defaultCardId,
+        },
+        data: {
+            isDefault: true,
+        }
+    })
 
     return NextResponse.json(1)
 }
