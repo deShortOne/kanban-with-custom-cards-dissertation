feb0416d17746acf844beeaaf65603afb1468386
deshortone <jingshianggu@gmail.com>
2024-03-03 17:31:51 +0000

    feat: added logout button not sure what else to add

--- app/(auth)/components/UserProfile.tsx
+++ app/(auth)/components/UserProfile.tsx
@@ -1,18 +1,14 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
     Sheet,
-    SheetClose,
     SheetContent,
-    SheetDescription,
-    SheetFooter,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet"
 import { Session } from "next-auth"
 import Image from "next/image"
+import { signOut } from "next-auth/react"
 
 export const UserAccount = ({ session }: { session: Session }) => {
     return (
@@ -26,32 +22,28 @@ export const UserAccount = ({ session }: { session: Session }) => {
                     className="rounded-full"
                 />
             </SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                    <SheetTitle>Edit profile</SheetTitle>
-                    <SheetDescription>
-                        Make changes to your profile here. Click save when you're done.
-                    </SheetDescription>
+            <SheetContent className="flex flex-col">
+                <SheetHeader className="flex">
+                    <div className="flex">
+                        <Image
+                            src={session.user.image as string}
+                            alt="User Setting button"
+                            width={32}
+                            height={32}
+                            className="rounded-full"
+                        />
+                        <SheetTitle className="ml-2">{session.user.name}</SheetTitle>
+                    </div>
                 </SheetHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Name
-                        </Label>
-                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
-                        </Label>
-                        <Input id="username" value="@peduarte" className="col-span-3" />
-                    </div>
+                <div className="flex-1" />
+                <div className="grid">
+                    <Button
+                        onClick={() => signOut()}
+                        variant="destructive"
+                    >
+                        Log out
+                    </Button>
                 </div>
-                <SheetFooter>
-                    <SheetClose asChild>
-                        <Button type="submit">Save changes</Button>
-                    </SheetClose>
-                </SheetFooter>
             </SheetContent>
         </Sheet>
     )
