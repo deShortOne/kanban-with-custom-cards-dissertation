ca9d82c2396976049573d00c71439bab0c6d8795
deshortone <jingshianggu@gmail.com>
2024-03-20 20:42:09 +0000

    fix: static apis ignore error ->
    Very much not how I would want this to work, but super tired...
    When doing next build to create the docker image, it attempts to contact a non existant database
    (Do want to say that the optimisation on this is turned up to 11 if it can detect if apis should be static or not)
    Future APIs with db interaction will check for db connection and handle it from there
    Also not every board will have every type of card available to them, it's in the works

--- app/api/card/fieldType/route.tsx
+++ app/api/card/fieldType/route.tsx
@@ -2,11 +2,15 @@ import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export async function GET() {
-    const fieldType = await prisma.fieldType.findMany({
-        where: {
-            implemented: true,
-        },
-    })
+    try {
+        const fieldType = await prisma.fieldType.findMany({
+            where: {
+                implemented: true,
+            },
+        })
 
-    return NextResponse.json(fieldType);
+        return NextResponse.json(fieldType)
+    } catch {
+        return NextResponse.error()
+    }
 }
