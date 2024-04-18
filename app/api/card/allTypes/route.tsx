9b9ed59578d968bc85203a58fb67ba2fe66e6651
deshortone <jingshianggu@gmail.com>
2024-04-18 14:07:19 +0100

    feat: api updates name of card type

--- app/api/card/allTypes/route.tsx
+++ app/api/card/allTypes/route.tsx
@@ -12,7 +12,52 @@ export async function GET() {
 export async function POST(req: Request) {
     const data = await req.json()
 
-    // TODO
+    const kanbanId = data["kanbanId"]
+    delete data["kanbanId"]
+
+    const oldCardTypeIds = Object.keys(data).filter(i => parseInt(i) > 0).map(i => parseInt(i))
+    const newCardTypes = Object.keys(data).filter(i => parseInt(i) > 0).map(i => parseInt(i))
+
+    const cardTypes = await prisma.cardType.findMany({
+        where: {
+            id: {
+                in: oldCardTypeIds
+            }
+        }
+    })
+
+    oldCardTypeIds.forEach(async (i) => {
+        if (cardTypes[i].name === data[i].name) {
+            return
+        }
+        let newCardId = -1
+        const preexistingCardType = await prisma.cardType.findFirst({
+            where: {
+                name: data[i].name
+            }
+        })
+
+        if (preexistingCardType === null) {
+            const newCardType = await prisma.cardType.create({
+                data: {
+                    name: data[i].name
+                }
+            })
+            newCardId = newCardType.id
+        } else {
+            newCardId = preexistingCardType.id
+        }
+
+        await prisma.cardTemplate.updateMany({
+            where: {
+                kanbanId: kanbanId,
+                cardTypeId: i
+            },
+            data: {
+                cardTypeId: newCardId
+            }
+        })
+    })
 
     return NextResponse.json(1)
 }
