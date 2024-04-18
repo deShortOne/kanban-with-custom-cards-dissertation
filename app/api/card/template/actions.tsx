5018f4e42e7ddd32f549f8fc6fac075420a12ab7
deshortone <jingshianggu@gmail.com>
2024-04-18 18:40:57 +0100

    fix: lint

---
+++ app/api/card/template/actions.tsx
@@ -0,0 +1,42 @@
+import { DataProp } from "@/app/(auth)/card/[id]/component/Base"
+import { prisma } from "@/lib/prisma"
+
+export async function insertTemplateTabsAndFields(data: DataProp, cardTemplateId: number) {
+    // Insert tabs
+    const insertTabs: any = data.tabs.map(i => ({
+        name: i.name,
+        order: i.order,
+        sizeX: i.sizeX,
+        sizeY: i.sizeY,
+        cardTemplateId: cardTemplateId,
+    }))
+    await prisma.cardTemplateTab.createMany({
+        data: insertTabs
+    })
+    const cardTabs = await prisma.cardTemplateTab.findMany({
+        where: {
+            cardTemplateId: cardTemplateId
+        }
+    })
+
+    // Insert tab fields
+    const insertTabFields = []
+    for (let i = 0; i < data.tabs.length; i++) {
+        const currTab = data.tabs[i]
+        const currTabDbId = cardTabs.find(i => i.name === currTab.name)?.id as number
+        for (let j = 0; j < currTab.tabFields.length; j++) {
+            if (currTab.tabFields[j].fieldType.id !== -1) {
+                insertTabFields.push({
+                    data: currTab.tabFields[j].data,
+                    posX: currTab.tabFields[j].posX,
+                    posY: currTab.tabFields[j].posY,
+                    fieldTypeId: currTab.tabFields[j].fieldType.id,
+                    cardTemplateTabId: currTabDbId,
+                })
+            }
+        }
+    }
+    await prisma.cardTemplateTabField.createMany({
+        data: insertTabFields
+    })
+}
